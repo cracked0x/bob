802a6db9bfcffb1b9d58cb026829235225033164
0xdeathstalker <soumyamukherjee.work@gmail.com>
2025-04-16 16:20:58 +0530

    chore: refactor

--- src/components/smooth-scroll.tsx
+++ src/components/smooth-scroll.tsx
@@ -42,102 +42,11 @@ const SmoothScroll = () => {
           className="relative flex h-screen w-max items-center"
         >
           <div className="relative flex">
-            {/* 1st screen */}
-            <div className="h-screen w-screen px-[140px]">
-              <div className="relative h-full">
-                <header>
-                  <div className="font-anticSlab 3xl:pt-[90px] flex items-center gap-[14px] place-self-end pt-12">
-                    <YellowButton>How it works</YellowButton>
-                    <YellowButton>Features</YellowButton>
-                  </div>
-                </header>
+            <FirstScreen />
 
-                <h1 className="font-fablab 3xl:mb-24 3xl:text-6xl mt-10 mb-16 text-5xl">BobTheBot</h1>
-                <p className="3xl:max-w-xl 3xl:text-4xl max-w-sm text-2xl font-bold 2xl:max-w-lg">
-                  Bob transforms web3 interaction through Telegram's familiar interface, removing technical barriers
-                  while preserving security.
-                </p>
+            <SecondScreen />
 
-                <Image
-                  src="/images/handle.png"
-                  alt="handle"
-                  width={65}
-                  height={100}
-                  className="3xl:w-[80px] pointer-events-none absolute bottom-6"
-                />
-                <Image
-                  src="/images/hook.png"
-                  alt="hook"
-                  width={180}
-                  height={1000}
-                  className="3xl:w-[230px] pointer-events-none absolute top-0 right-[40%] 2xl:w-[200px]"
-                />
-                {/* LOOP AND ARROW */}
-                <div className="3xl:top-[35%] 3xl:right-[25%] absolute top-[30%] right-1/5 2xl:top-[33%] 2xl:right-[23%]">
-                  <div className="relative">
-                    <Image
-                      src="/images/clickonstart.svg"
-                      alt="click here to start"
-                      width={271}
-                      height={156}
-                      className="w-56"
-                    />
-                    <Image
-                      src="/images/arrow.svg"
-                      alt="arrow"
-                      width={143}
-                      height={116}
-                      className="mt-5"
-                    />
-                  </div>
-                </div>
-                <Image
-                  src="/images/bob.png"
-                  alt="bob the bot image"
-                  width={200}
-                  height={350}
-                  className="3xl:w-[250px] pointer-events-none absolute right-24 bottom-1"
-                />
-              </div>
-            </div>
-
-            {/* 2nd screen */}
-            <div className="flex h-screen w-screen justify-center px-[140px]">
-              <Image
-                src="/images/grid.png"
-                alt="a table of information"
-                width={1412}
-                height={300}
-                className="pointer-events-none relative place-self-end"
-              />
-            </div>
-
-            {/* 3rd screen */}
-            <div className="relative h-screen w-screen px-[140px]">
-              <h2 className="3xl:mt-32 3xl:text-4xl 3xl:max-w-3xl mt-24 max-w-2xl text-2xl font-bold 2xl:mt-24 2xl:max-w-2xl 2xl:text-3xl">
-                Traditional web3 bots compromise security for convenience. Bob delivers both - the seamless experience
-                of a Telegram bot with the security guarantees of self-custodial solutions.
-              </h2>
-
-              <div className="absolute bottom-5">
-                <img
-                  src="/images/arrow-2.svg"
-                  alt="arrow"
-                  className="absolute top-0 left-84 2xl:-top-10 2xl:left-96"
-                />
-                <img
-                  src="/images/bob-on-truck.png"
-                  alt="bob on truck"
-                  className="3xl:w-md w-sm"
-                />
-              </div>
-
-              <img
-                src="/images/truck.png"
-                alt="a truck"
-                className="absolute right-0 bottom-0 h-screen"
-              />
-            </div>
+            <ThirdScreen />
           </div>
         </motion.section>
       </div>
@@ -166,6 +75,118 @@ const SmoothScroll = () => {
         className="fixed bottom-0 w-screen"
       />
     </>
+  );
+};
+
+const Header = () => {
+  return (
+    <header>
+      <div className="font-anticSlab 3xl:pt-[90px] flex items-center gap-[14px] place-self-end pt-12">
+        <YellowButton>How it works</YellowButton>
+        <YellowButton>Features</YellowButton>
+      </div>
+    </header>
+  );
+};
+
+const FirstScreen = () => {
+  return (
+    <div className="h-screen w-screen px-[140px]">
+      <div className="relative h-full">
+        <Header />
+
+        <h1 className="font-fablab 3xl:mb-24 3xl:text-6xl mt-10 mb-16 text-5xl">BobTheBot</h1>
+        <p className="3xl:max-w-xl 3xl:text-4xl max-w-sm text-2xl font-bold 2xl:max-w-lg">
+          Bob transforms web3 interaction through Telegram's familiar interface, removing technical barriers while
+          preserving security.
+        </p>
+
+        <Image
+          src="/images/handle.png"
+          alt="handle"
+          width={65}
+          height={100}
+          className="3xl:w-[80px] pointer-events-none absolute bottom-6"
+        />
+        <Image
+          src="/images/hook.png"
+          alt="hook"
+          width={180}
+          height={1000}
+          className="3xl:w-[230px] pointer-events-none absolute top-0 right-[40%] 2xl:w-[200px]"
+        />
+        {/* LOOP AND ARROW */}
+        <div className="3xl:top-[35%] 3xl:right-[25%] absolute top-[30%] right-1/5 2xl:top-[33%] 2xl:right-[23%]">
+          <div className="relative">
+            <Image
+              src="/images/clickonstart.svg"
+              alt="click here to start"
+              width={271}
+              height={156}
+              className="w-56"
+            />
+            <Image
+              src="/images/arrow.svg"
+              alt="arrow"
+              width={143}
+              height={116}
+              className="mt-5"
+            />
+          </div>
+        </div>
+        <Image
+          src="/images/bob.png"
+          alt="bob the bot image"
+          width={200}
+          height={350}
+          className="3xl:w-[250px] pointer-events-none absolute right-24 bottom-1"
+        />
+      </div>
+    </div>
+  );
+};
+
+const SecondScreen = () => {
+  return (
+    <div className="flex h-screen w-screen justify-center px-[140px]">
+      <Image
+        src="/images/grid.png"
+        alt="a table of information"
+        width={1412}
+        height={300}
+        className="pointer-events-none relative place-self-end"
+      />
+    </div>
+  );
+};
+
+const ThirdScreen = () => {
+  return (
+    <div className="relative h-screen w-screen px-[140px]">
+      <h2 className="3xl:mt-32 3xl:text-4xl 3xl:max-w-3xl mt-24 max-w-2xl text-2xl font-bold 2xl:mt-24 2xl:max-w-2xl 2xl:text-3xl">
+        Traditional web3 bots compromise security for convenience. Bob delivers both - the seamless experience of a
+        Telegram bot with the security guarantees of self-custodial solutions.
+      </h2>
+
+      <div className="absolute bottom-5">
+        <img
+          src="/images/arrow-2.svg"
+          alt="arrow"
+          className="absolute top-0 left-84 2xl:-top-10 2xl:left-96"
+        />
+        <img
+          src="/images/bob-on-truck.png"
+          alt="bob on truck"
+          className="3xl:w-md w-sm"
+        />
+      </div>
+
+      <img
+        src="/images/truck.png"
+        alt="a truck"
+        className="absolute right-0 bottom-0 h-screen"
+      />
+    </div>
   );
 };
 
