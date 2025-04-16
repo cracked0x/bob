daf9118ac17791956bab93d1f25aed867f843ec5
0xdeathstalker <soumyamukherjee.work@gmail.com>
2025-04-16 15:55:49 +0530

    chore: format

--- src/components/smooth-scroll.tsx
+++ src/components/smooth-scroll.tsx
@@ -63,14 +63,14 @@ const SmoothScroll = () => {
                   alt="handle"
                   width={65}
                   height={100}
-                  className="3xl:w-[80px] absolute bottom-6 pointer-events-none"
+                  className="3xl:w-[80px] pointer-events-none absolute bottom-6"
                 />
                 <Image
                   src="/images/hook.png"
                   alt="hook"
                   width={180}
                   height={1000}
-                  className="3xl:w-[230px] absolute top-0 right-[40%] 2xl:w-[200px] pointer-events-none"
+                  className="3xl:w-[230px] pointer-events-none absolute top-0 right-[40%] 2xl:w-[200px]"
                 />
                 {/* LOOP AND ARROW */}
                 <div className="3xl:top-[35%] 3xl:right-[25%] absolute top-[30%] right-1/5 2xl:top-[33%] 2xl:right-[23%]">
@@ -96,7 +96,7 @@ const SmoothScroll = () => {
                   alt="bob the bot image"
                   width={200}
                   height={350}
-                  className="3xl:w-[250px] absolute right-24 bottom-1 pointer-events-none"
+                  className="3xl:w-[250px] pointer-events-none absolute right-24 bottom-1"
                 />
               </div>
             </div>
@@ -108,20 +108,35 @@ const SmoothScroll = () => {
                 alt="a table of information"
                 width={1412}
                 height={300}
-                className="relative place-self-end pointer-events-none"
+                className="pointer-events-none relative place-self-end"
               />
             </div>
 
             {/* 3rd screen */}
-            <div className="h-screen w-screen relative px-[140px]">
-              <h2 className="mt-24 2xl:mt-24 3xl:mt-32 2xl:text-3xl 3xl:text-4xl max-w-2xl text-2xl font-bold 2xl:max-w-2xl 3xl:max-w-3xl">Traditional web3 bots compromise security for convenience. Bob delivers both - the seamless experience of a Telegram bot with the security guarantees of self-custodial solutions.</h2>
-              
+            <div className="relative h-screen w-screen px-[140px]">
+              <h2 className="3xl:mt-32 3xl:text-4xl 3xl:max-w-3xl mt-24 max-w-2xl text-2xl font-bold 2xl:mt-24 2xl:max-w-2xl 2xl:text-3xl">
+                Traditional web3 bots compromise security for convenience. Bob delivers both - the seamless experience
+                of a Telegram bot with the security guarantees of self-custodial solutions.
+              </h2>
+
               <div className="absolute bottom-5">
-                <img src="/images/arrow-2.svg" alt="arrow" className="absolute top-0 2xl:-top-10 left-84 2xl:left-96" />
-                <img src="/images/bob-on-truck.png" alt="bob on truck" className="w-sm 3xl:w-md"/>
+                <img
+                  src="/images/arrow-2.svg"
+                  alt="arrow"
+                  className="absolute top-0 left-84 2xl:-top-10 2xl:left-96"
+                />
+                <img
+                  src="/images/bob-on-truck.png"
+                  alt="bob on truck"
+                  className="3xl:w-md w-sm"
+                />
               </div>
-              
-              <img src="/images/truck.png" alt="a truck" className="absolute h-screen bottom-0 right-0" />
+
+              <img
+                src="/images/truck.png"
+                alt="a truck"
+                className="absolute right-0 bottom-0 h-screen"
+              />
             </div>
           </div>
         </motion.section>
@@ -136,14 +151,14 @@ const SmoothScroll = () => {
         alt="background"
         width={5000}
         height={5000}
-        className="fixed bottom-0 -z-10 pointer-events-none"
+        className="pointer-events-none fixed bottom-0 -z-10"
       />
       <Image
         src="/images/grain.png"
         alt="grain background"
         width={5000}
         height={5000}
-        className="fixed top-0 -z-10 opacity-60 pointer-events-none"
+        className="pointer-events-none fixed top-0 -z-10 opacity-60"
       />
       <CautionTape
         stripeWidth={20}
